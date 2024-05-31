1010f3a87ba95563bb9c2345f3fd5debd761022c
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-06-01 00:22:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If requiredfunction
 function removeChar(str){
 	str=str.toLowerCase()
-	str=str.replaceAll(" the "," ")
-	str=str.replaceAll(" a "," ")
-	str=str.replaceAll(" an "," ")
+	str=str.replaceAll(" the ","")
+	str=str.replaceAll(" a ","")
+	str=str.replaceAll(" an ","")
 	return str
 }
 bands.sort((a,b)=>{
