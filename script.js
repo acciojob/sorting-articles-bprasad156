ffb8a5d54785707d87e515ed4e3b3dbe430051bf
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-06-01 00:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 //your JS code here. If requiredfunction
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 
+    'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 
+    'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 
+    'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
 
-// xyx ram apple 
 
-function removeChar(str){
-    str =str + " "
-    str = str.toLowerCase()
-    str = str.replaceAll(" the "," ")
-    str = str.replaceAll(" a "," ")
-    str = str.replaceAll(" an "," ")
-    return str.trim()
-    
+function stripArticle(bandName) {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
 }
 
 
-bands.sort((a,b)=>{
+const sortedBands = bands.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
 
-   if(removeChar(a) > removeChar(b)){
-       return 1
-    }
-    else{
-        return -1
-    }
 
-})
-
-console.log(bands)
+console.log(sortedBands);
