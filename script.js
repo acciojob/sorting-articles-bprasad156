bba00eee2228ea203309f71fc30f76cc45bd26da
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-06-01 00:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 //your JS code here. If requiredfunction
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+// xyx ram apple 
+
 function removeChar(str){
-	str=str.toLowerCase()
-	str=str.replaceAll(" the ","")
-	str=str.replaceAll(" a ","")
-	str=str.replaceAll(" an ","")
-	return str
+    str = " "+ str + " "
+    str = str.toLowerCase()
+    str = str.replaceAll(" the "," ")
+    str = str.replaceAll(" a "," ")
+    str = str.replaceAll(" an "," ")
+    return str.trim()
+    
 }
+
+
 bands.sort((a,b)=>{
-	if(removeChar(a)>removeChar(b)){
-		return 
-	}
-	else{
-		return 
-	}
+
+   if(removeChar(a) > removeChar(b)){
+       return 1
+    }
+    else{
+        return -1
+    }
+
 })
+
+console.log(bands)
