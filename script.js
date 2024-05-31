64d6d1559b1ba93a0a65c5c98eff73e09984d1d7
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-06-01 01:11:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 //your JS code here. If requiredfunction
+
+
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-
-// xyx ram apple 
-
 function removeChar(str){
-    str = str + " ";
-    str = str.toLowerCase();
-    str = str.replace(/ the | a | an /g, " ");
-    return str.trim();
-}
+    return str.replace(/^(the |a |an )/i, '').trim();
 bands.sort((a,b)=>{
-
    if(removeChar(a) > removeChar(b)){
-       return 1
+       return 1;
     }
     else{
-        return -1
+        return -1;
     }
-
 })
 
-console.log(bands)
+console.log(bands);
