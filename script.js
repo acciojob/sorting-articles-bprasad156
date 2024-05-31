3cd32ebc54199341309a65c26b814329db92779e
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-06-01 00:09:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-//your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-bands.sort();
-console.log(bands);
+//your JS code here. If requiredfunction
+function removeChar(str){
+	str=str.toLowerCase()
+	str=str.replaceAll(" the ","")
+	str=str.replaceAll(" a ","")
+	str=str.replaceAll(" an ","")
+	return str
+}
+bands.sort((a,b)=>{
+	if(removeChar(a)>removeChar(b)){
+		return 
+	}
+	else{
+		return 
+	}
+})
